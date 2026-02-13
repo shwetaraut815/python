d302ee582c078cc8cefb727bbf8bde804e44f931
shwetaraut815 <shwetaraut815@gmail.com>
2026-02-13 17:01:40 +0000

    day10

---
+++ day10.py
@@ -0,0 +1,15 @@
+## imput from user
+
+a = input()
+print (a)
+
+b = input("mwhat is your age :: ")     ### bydefault taken input as a string
+print ("my name is", b)
+
+x=(input("enter 1st number:::"))
+### input with type casting
+var1=int(input())
+var2=float(input())
+
+## or you can use 
+print (int(var1)+int(var2))
