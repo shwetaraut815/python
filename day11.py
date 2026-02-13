770b57ebed52243635ac9462a2d691af021d0713
shwetaraut815 <shwetaraut815@gmail.com>
2026-02-13 18:15:40 +0000

    day 11 string

---
+++ day11.py
@@ -0,0 +1,34 @@
+
+### string class
+
+a="hello"
+b='hello'
+
+c='''how are you
+are you ok
+got it'''
+
+d="""helloo
+hdhsu
+jhdsuh
+dywu"""
+
+
+## indexing
+
+x="japan"
+
+print(x[0])
+print(x[1])
+print(x[3])
+
+## looping through the string
+
+for character in c:
+    print(character)
+    
+
+for character in x:
+    print(character)
+    
+    
