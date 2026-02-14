72df0eb76643fcf46636b37aec2e3e32e7e41fe1
shwetaraut815 <shwetaraut815@gmail.com>
2026-02-14 01:52:34 +0000

    day 12

---
+++ day12.py
@@ -0,0 +1,23 @@
+### string slicing and operations
+
+a = "kapila, rain"
+print (a[0:9])
+
+print (len(a))    ## length function
+
+## slicing 
+name = "rutujaratat"
+print (len(name))
+print (name[2:7])   ## including 2 but not a 7
+print (name[:5])  #taken fron zero
+print (name[3:-4])  #negative slicing  consider it as a "len(name)-4"
+print (name[:-6])
+
+print (name[-3:-1])  ##len(name)-3:len(name)-1
+
+## ques
+nm = "harry"
+print (len(nm))  ### length start from 1
+print (nm[-4:-2])  ### indexing start from zero
+
+
